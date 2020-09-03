c025c36327a2bff3b3e01c50ea4e71c06d8229d5
Michael Ren <michael_ren@berkeley.edu>
2020-09-02 21:29:28 -0700

    Updates

---
+++ precache-manifest.c3dd5d4eefbbe91318df42c538d84cd7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4059774c6c82da8cb8d7828cb8f9f5a2",
+    "url": "/michaelren.me/index.html"
+  },
+  {
+    "revision": "6548fec1524e22d733c4",
+    "url": "/michaelren.me/static/css/main.77c5fc6b.chunk.css"
+  },
+  {
+    "revision": "a7951f6a5405e87200b6",
+    "url": "/michaelren.me/static/js/2.ea787863.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/michaelren.me/static/js/2.ea787863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6548fec1524e22d733c4",
+    "url": "/michaelren.me/static/js/main.9423097f.chunk.js"
+  },
+  {
+    "revision": "9330917dbe4d4da4403b",
+    "url": "/michaelren.me/static/js/runtime-main.e7be74e4.js"
+  },
+  {
+    "revision": "deaeae06d5370d59ceeacfd1120f0197",
+    "url": "/michaelren.me/static/media/cal1card.deaeae06.jpg"
+  },
+  {
+    "revision": "a7cde09f87773accae656f6fc3c4932a",
+    "url": "/michaelren.me/static/media/garbagebegone.a7cde09f.jpg"
+  },
+  {
+    "revision": "aa14c4d0c2d7cf33275ff8f9a24de0e4",
+    "url": "/michaelren.me/static/media/melter.aa14c4d0.png"
+  },
+  {
+    "revision": "f57ce61cb5f02bad9f072fc1524af57d",
+    "url": "/michaelren.me/static/media/pathtracer.f57ce61c.png"
+  },
+  {
+    "revision": "0bfe45453b193d4f651be40cf7e9bb59",
+    "url": "/michaelren.me/static/media/profile-circle.0bfe4545.png"
+  }
+]);
